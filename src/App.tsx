c4260dcd45815c2a27fd54820c99f48057a74d2e
Víctor Oliva <olivarra1@gmail.com>
2018-11-28 20:31:20 +0100

    Update libraries

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import { KeyboardAvoidingView, StyleSheet } from 'react-native';
 import { NavigationContainerComponent } from 'react-navigation';
 import AppNavigator, { setTopLevelNavigator } from './navigation';
 import { Provider } from './redux';
-import { Font } from 'expo';
+// import { Font } from 'expo';
 
 const handleNavigatorRef = (ref: NavigationContainerComponent) => setTopLevelNavigator(ref);
 const App = () => (<AppNavigator ref={handleNavigatorRef} />)
 
 export default class extends React.Component {
     componentDidMount() {
-        Font.loadAsync({
-            'share-tech-mono': require('../assets/fonts/ShareTechMono-Regular.ttf'),
-        });
+        // Font.loadAsync({
+        //     'share-tech-mono': require('../assets/fonts/ShareTechMono-Regular.ttf'),
+        // });
     }
 
     render = () => (
