449e17f02fd153e80a90e45bea5665649f865271
Víctor Oliva <olivarra1@gmail.com>
2018-12-04 08:24:33 +0100

    Eject by react-native-init

--- App.js
+++
@@ -1,4 +0,0 @@
-import App from './build/App';
-import 'cryptojslib/rollups/aes';
-
-export default App;
---
+++ index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './build/App';
+import {name as appName} from './app.json';
+import 'cryptojslib/rollups/aes';
+
+AppRegistry.registerComponent(appName, () => App);
