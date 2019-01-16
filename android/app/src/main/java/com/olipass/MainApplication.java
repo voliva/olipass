21bbdea3259fcf9698ff501aa11ff1299856db3c
Víctor Oliva <olivarra1@gmail.com>
2019-01-16 19:52:42 +0100

    WIP open/save files

--- android/app/src/main/java/com/olipass/MainApplication.java
+++ android/app/src/main/java/com/olipass/MainApplication.java
@@ -3,6 +3,7 @@ package com.olipass;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
 import io.realm.react.RealmReactPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeDocumentPicker(),
             new RealmReactPackage(),
             new RandomBytesPackage()
       );
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,26 @@ import AppNavigator, { setTopLevelNavigator } from './navigation';
 import { Provider } from './redux';
 import { connect } from 'react-redux';
 
+import { DocumentPicker, DocumentPickerUtil } from 'react-native-document-picker';
+
+DocumentPicker.show({
+    filetype: [DocumentPickerUtil.allFiles()],
+}, (error: any,res: any) => {
+    if(error) {
+        console.warn(error);
+        return;
+    }
+
+    console.log(
+        res.uri,
+        res.type, // mime type
+        res.fileName,
+        res.fileSize,
+        res
+    );
+});
+
+
 const handleNavigatorRef = (ref: NavigationContainerComponent) => setTopLevelNavigator(ref);
 
 const App = connect(null,
