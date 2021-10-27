8edf5c02814458debde253305de4a103b938382f
Víctor Oliva <olivarra1@gmail.com>
2021-10-27 15:30:15 +0200

    prepare for demo

--- src/modules/auth/auth.ts
+++ src/modules/auth/auth.ts
@@ -1,6 +1,6 @@
-import { bind } from "@react-rxjs/core";
+import { shareLatest } from "@react-rxjs/core";
 import { merge, Subject } from "rxjs";
-import { filter, map, withLatestFrom, tap } from "rxjs/operators";
+import { filter, map, tap, withLatestFrom } from "rxjs/operators";
 import { getScreenRoutePath, history, Screen } from "src/router";
 import { DB, loadDB, upsertDB } from "src/services/encryptedDB";
 
@@ -17,75 +17,71 @@ export function initialize() {
 export const authLogin = new Subject<string>();
 export const authCreate = new Subject<string>();
 
-const [, login$] = bind(
-  authLogin.pipe(
-    map((password) => {
-      try {
-        const database = loadDB(password);
-        return {
-          result: "success" as const,
-          database,
-        };
-      } catch (ex) {
-        console.error(ex);
-        return {
-          result: "error" as const,
-        };
-      }
-    })
-  )
-);
-
-const [, create$] = bind(
-  authCreate.pipe(
-    map((password) => {
-      const database: DB = {
-        sites: [],
-        version: 1,
-      };
-      upsertDB(database, password);
+const login$ = authLogin.pipe(
+  map((password) => {
+    try {
+      const database = loadDB(password);
       return {
         result: "success" as const,
         database,
       };
-    })
-  )
+    } catch (ex) {
+      console.error(ex);
+      return {
+        result: "error" as const,
+      };
+    }
+  }),
+  shareLatest()
 );
 
-const [, loginResult$] = bind(
-  login$.pipe(
-    filter(({ result }) => result === "success"),
-    withLatestFrom(authLogin),
-    map(([login, password]) => ({
-      database: login.database as DB,
-      password,
-    }))
-  )
-);
-const [, createResult$] = bind(
-  create$.pipe(
-    filter(({ result }) => result === "success"),
-    withLatestFrom(authCreate),
-    map(([creation, password]) => ({
-      database: creation.database,
-      password,
-    }))
-  )
+const create$ = authCreate.pipe(
+  map((password) => {
+    const database: DB = {
+      sites: [],
+      version: 1,
+    };
+    upsertDB(database, password);
+    return {
+      result: "success" as const,
+      database,
+    };
+  })
 );
 
-export const [, password$] = bind(
-  merge(loginResult$, createResult$).pipe(map(({ password }) => password))
+const loginResult$ = login$.pipe(
+  filter(({ result }) => result === "success"),
+  withLatestFrom(authLogin),
+  map(([login, password]) => ({
+    database: login.database as DB,
+    password,
+  }))
 );
-export const [, loginDB$] = bind(
-  merge(loginResult$, createResult$).pipe(map(({ database }) => database))
-);
-export const [, error$] = bind(
-  login$.pipe(
-    filter(({ result }) => result === "error"),
-    map(() => void 0)
-  )
+const createResult$ = create$.pipe(
+  filter(({ result }) => result === "success"),
+  withLatestFrom(authCreate),
+  map(([creation, password]) => ({
+    database: creation.database,
+    password,
+  }))
 );
 
-export const [, authRedirect] = bind(
-  password$.pipe(tap(() => history.replace(getScreenRoutePath(Screen.Main))))
+const authResult$ = merge(loginResult$, createResult$).pipe(shareLatest());
+
+export const password$ = authResult$.pipe(
+  map(({ password }) => password),
+  shareLatest()
+);
+export const loginDB$ = authResult$.pipe(
+  map(({ database }) => database),
+  shareLatest()
+);
+export const error$ = login$.pipe(
+  filter(({ result }) => result === "error"),
+  map(() => void 0),
+  shareLatest()
+);
+
+export const authRedirect = password$.pipe(
+  tap(() => history.replace(getScreenRoutePath(Screen.Main)))
 );
